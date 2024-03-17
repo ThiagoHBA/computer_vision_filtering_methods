e8991efa996475f294bf06f0396fd57e31046625
ThiagoHBA <thiagohhab@gmail.com>
2024-03-17 16:02:00 -0300

    adding median filter + adding gaussian filter

--- basic_computer_vision_methods.py
+++ basic_computer_vision_methods.py
@@ -1,4 +1,5 @@
 from cgi import test
+from unittest import result
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -13,8 +14,14 @@ def main():
     cv2.imshow('ORIGINAL IMAGE', np.array(pixels_matrix, dtype=np.uint8))
 
     # LOW PASS
-    cv2.imshow('LOW PASS MEAN FILTER', np.array(perform_low_pass_filter_mean(pixels_matrix), dtype=np.uint8))
-    cv2.imshow('LOW PASS MEAN FILTER BASE', np.array(perform_low_pass_filter_opencv(pixels_matrix), dtype=np.uint8))
+    # cv2.imshow('LOW PASS MEAN FILTER', np.array(perform_low_pass_filter_mean(pixels_matrix), dtype=np.uint8))
+    # cv2.imshow('LOW PASS MEAN FILTER BASE', np.array(perform_median_filter_opencv(pixels_matrix), dtype=np.uint8))
+
+    # cv2.imshow('LOW PASS MEDIAN FILTER', np.array(perform_low_pass_filter_median(pixels_matrix), dtype=np.uint8))
+    # cv2.imshow('LOW PASS MEDIAN FILTER BASE', np.array(perform_median_filter_opencv(pixels_matrix), dtype=np.uint8))
+
+    cv2.imshow('LOW PASS GAUSSIAN FILTER', np.array(perform_low_pass_filter_gaussian(pixels_matrix), dtype=np.uint8))
+    cv2.imshow('LOW PASS GAUSSIAN FILTER BASE', np.array(perform_gaussian_filter_opencv(pixels_matrix), dtype=np.uint8))
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -31,8 +38,38 @@ def perform_low_pass_filter_mean(pixels_matrix):
         [1, 1, 1, 1, 1],
     ]
 
-    kernel_center_row = len(kernel) // 2
-    kernel_center_col = len(kernel[0]) // 2
+    result_matrix = []
+
+    for _ in range(len(pixels_matrix)):
+        result_matrix.append([])
+
+    for i in range(len(pixels_matrix)):
+        for j in range(len(pixels_matrix[0])):
+            colision_matrix = calculate_colision_matrix(pixels_matrix, kernel, i, j)
+
+            result_matrix[i].append(calculate_matrix_mean(colision_matrix))
+    
+    return result_matrix
+
+# BASE
+def perform_mean_filter_opencv(pixels_matrix):
+    image = np.array(pixels_matrix, dtype=np.uint8)
+    kernel = np.ones((5,5),np.float32)/25
+    image_filtered = cv2.filter2D(image,-1,kernel)
+
+    return image_filtered
+
+''' ================= MEDIAN FILTER ======================'''
+
+# IMPLEMENTATION
+def perform_low_pass_filter_median(pixels_matrix):
+    kernel = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+    ]
 
     result_matrix = []
 
@@ -41,41 +78,113 @@ def perform_low_pass_filter_mean(pixels_matrix):
 
     for i in range(len(pixels_matrix)):
         for j in range(len(pixels_matrix[0])):
-            colision_matrix = []
+            colision_matrix = calculate_colision_matrix(pixels_matrix, kernel, i, j)
 
+            bubbleSort(colision_matrix)
+            middle_element = len(colision_matrix) // 2
+            result_matrix[i].append(colision_matrix[middle_element])
+    
+    return result_matrix
+
+# BASE
+def perform_median_filter_opencv(pixels_matrix):
+    image = np.array(pixels_matrix, dtype=np.uint8)
+    kernel_size = 5  # Change this value as needed
+    image_filtered = cv2.medianBlur(image, kernel_size)
+
+    return image_filtered
+
+''' ================= GAUSSIAN FILTER ======================'''
+
+# IMPLEMENTATION
+def perform_low_pass_filter_gaussian(pixels_matrix):
+    kernel = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+    ]
+
+    kernel_center = len(kernel) // 2
+    pi = 3.1415
+    sigma = 1
+    e = 2.71828
+
+    for x in range(len(kernel)):
+        for y in range(len(kernel[0])):
+            horizontal_offset = (x - kernel_center)
+            vertical_offset = (y - kernel_center)
+
+            diff = (horizontal_offset ** 2 + vertical_offset ** 2) / (2 * (sigma ** 2))
+            kernel[x][y] = (1 / (2 * pi * sigma ** 2)) * (e ** -diff)
+
+    result_matrix = []
+
+    for _ in range(len(pixels_matrix)):
+        result_matrix.append([])
+
+    kernel_center_row = len(kernel) // 2
+    kernel_center_col = len(kernel[0]) // 2
+
+    for i in range(len(pixels_matrix)):
+        for j in range(len(pixels_matrix[0])):
+            value = 0
             for m in range(len(kernel)):
                 for n in range(len(kernel[0])):
                     row = i + (m - kernel_center_row)
                     col = j + (n - kernel_center_col)
 
                     if 0 <= row < len(pixels_matrix) and 0 <= col < len(pixels_matrix[0]):
-                        colision_matrix.append(pixels_matrix[row][col])
+                        value += pixels_matrix[row][col] * kernel[m][n]
 
-            result_matrix[i].append(calculate_matrix_mean(colision_matrix))
-    
+            result_matrix[i].append(value)
     return result_matrix
 
 # BASE
-def perform_low_pass_filter_opencv(pixels_matrix):
+def perform_gaussian_filter_opencv(pixels_matrix):
     image = np.array(pixels_matrix, dtype=np.uint8)
-    kernel = np.ones((5,5),np.float32)/25
-    dst = cv2.filter2D(image,-1,kernel)
+    kernel_size = (5, 5)
+    sigmaX = 0
+    image_filtered = cv2.GaussianBlur(image, kernel_size, sigmaX)
 
-    return dst
-
-''' =================================================== '''
+    return image_filtered
 
 ''' ===================== HELPERS ===================== '''
 
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        if not swapped:
+            return
+
 def calculate_matrix_mean(matrix):
     sum = 0
-
     for i in matrix:
         sum += i
     
     return sum / len(matrix)
 
-    
+def calculate_colision_matrix(matrix, kernel, baseRow, baseCol):
+    colision_matrix = []
+    kernel_center_row = len(kernel) // 2
+    kernel_center_col = len(kernel[0]) // 2
+
+    for m in range(len(kernel)):
+        for n in range(len(kernel[0])):
+            row = baseRow + (m - kernel_center_row)
+            col = baseCol + (n - kernel_center_col)
+
+            if 0 <= row < len(matrix) and 0 <= col < len(matrix[0]):
+                colision_matrix.append(matrix[row][col])
+
+    return colision_matrix
 
 if __name__ == '__main__':
     main()
